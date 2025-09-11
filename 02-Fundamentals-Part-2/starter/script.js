66579be993974e05e954c268d940c717139552c0
Chu A. <chu.amk22@gmail.com>
2025-09-12 00:38:39 +0100

    Exercise on arrays and array methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -81,18 +81,57 @@
 // console.log(friends[friends.length - 1]);
 
 // Basic array methods
-const friends = ["Peter", "Brian", "Meg"];
-//add elements
-friends.push("Carl"); // add Carl at end of array
-console.log(friends);
-friends.unshift("Olson");
-console.log(friends); // add Olson at start of array
+// const friends = ["Peter", "Brian", "Meg"];
+// //add elements
+// friends.push("Carl"); // add Carl at end of array
+// console.log(friends);
+// friends.unshift("Olson");
+// console.log(friends); // add Olson at start of array
 
-//remove elements
-friends.pop();
-console.log(friends);
-friends.shift();
-console.log(friends);
+// //remove elements
+// friends.pop();
+// console.log(friends);
+// friends.shift();
+// console.log(friends);
 
-console.log(friends.indexOf("Brian"));
-console.log(friends.includes("Brian"));
+// console.log(friends.indexOf("Brian"));
+// console.log(friends.includes("Brian"));
+
+/**
+ * Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+And now let's use arrays! So, create an array called bills containing the test data below.
+
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44.
+ */
+
+const calcTip = (bill) => {
+  let tip;
+  if (bill >= 50 && bill <= 300) {
+    tip = bill * (15 / 100);
+  } else {
+    tip = bill * (20 / 100);
+  }
+  return tip;
+};
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [
+  bills[0] + calcTip(bills[0]),
+  bills[1] + calcTip(bills[1]),
+  bills[2] + calcTip(bills[2]),
+];
+
+console.log(tips);
+console.log(total);
