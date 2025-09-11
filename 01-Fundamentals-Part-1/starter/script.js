da3adf8e64e588762ab12e8ccb81c4d65d5f8c7d
Chu A. <chu.amk22@gmail.com>
2025-09-11 20:42:39 +0100

    Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,38 @@
-let name = "Chu Abuemkeze";
-let age = 22;
-let isAtRebase = true;
+// let name = "Chu Abuemkeze";
+// let age = 22;
+// let isAtRebase = true;
 
-console.log(name, age, isAtRebase);
+// console.log(name, age, isAtRebase);
+
+// Math Operators
+const currentYear = 2025;
+const ageChu = currentYear - 2003;
+const ageBecky = currentYear - 2001;
+console.log(ageChu, ageBecky);
+
+const firstName = "Mueghe";
+const middleName = "Abuemkeze";
+const lastName = "Chu";
+
+console.log(firstName + " " + middleName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+console.log(x);
+
+// Truthy and Falsy values
+
+// JS has 5 falsy values: 0, "", undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+// Truthy values
+console.log(Boolean(12));
+console.log(Boolean("Hello"));
+console.log(Boolean({}));
