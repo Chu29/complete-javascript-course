313f3ee66ffb1a287092e71fb9ba0298cd8cfb51
Chu A. <chu.amk22@gmail.com>
2025-09-11 22:17:43 +0100

    Functions and function parameters

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,19 @@
+"use strict";
+function logger() {
+  console.log("Hello, I'm Chu!");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(3, 2);
+console.log(appleOrangeJuice);
