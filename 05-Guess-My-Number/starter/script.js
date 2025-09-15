aa4befcd09daa061261b8d9a2b2aa2f3aa2fa363
Chu A. <chu.amk22@gmail.com>
2025-09-15 23:24:03 +0100

    chore(feat): implementing highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,11 +14,11 @@ console.log(document.querySelector('.guess').value);
 // generate the number
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 // click event to check the guess
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   // when there is no input
   if (!guess) {
@@ -26,10 +26,16 @@ document.querySelector('.check').addEventListener('click', () => {
 
     // when player wins the game
   } else if (guess === secretNumber) {
-    document.querySelector('.number').textContent = secretNumber;
+    // implementing the high score
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
     document.querySelector('.message').textContent = '🎉 Correct Number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
 
     // when guess is too high
   } else if (guess > secretNumber) {
