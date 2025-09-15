e6f275a31cc93113c1f3862a1aafc6663a8713a7
Chu A. <chu.amk22@gmail.com>
2025-09-15 20:08:47 +0100

    selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,3 +2,10 @@
 
 const msg = document.querySelector('.message').textContent;
 console.log(msg);
+
+document.querySelector('.message').textContent = '🎉 Correct Number';
+
+document.querySelector('.number').textContent = 30;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value);
