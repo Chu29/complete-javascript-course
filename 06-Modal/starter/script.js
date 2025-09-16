2a6ff5f8c1a367480b60f8d17d3128ac88537996
Chu A. <chu.amk22@gmail.com>
2025-09-16 10:17:02 +0100

    chore(refactor): refactor the code making it DRY

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,16 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
+// function to open modal
+const closeModal = () => {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+
+// function to close modal
+const openModal = () => {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+};
+
 // handle show modal click event
-btnsOpenModal.forEach(button => {
-  button.addEventListener('click', () => {
-    overlay.classList.remove('hidden');
-    modal.classList.remove('hidden');
-  });
+btnsOpenModal.forEach((button) => {
+  button.addEventListener('click', openModal);
 });
 
 // handle close model event
-btnCloseModal.addEventListener('click', () => {
-  overlay.classList.add('hidden');
-  modal.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
