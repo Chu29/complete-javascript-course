6f70ffaa9d4446d033e0881dcc7d9b899a458f82
Chu A. <chu.amk22@gmail.com>
2025-09-16 09:37:31 +0100

    chore(feat): opening and closing the modal window

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+// handle show modal click event
+btnsOpenModal.forEach(button => {
+  button.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+    modal.classList.remove('hidden');
+  });
+});
+
+// handle close model event
+btnCloseModal.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+});
