ad1dae54673c55d01708112ff65b6901454d07a5
Chu A. <chu.amk22@gmail.com>
2025-09-21 20:26:12 +0100

    array and object destructuring, the spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -30,6 +30,20 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex - 1], this.mainMenu[mainIndex - 1]];
+  },
+  orderDelivery: function ({
+    starterIndex = 0,
+    mainIndex = 0,
+    time = '00:00',
+    address = 'main street index',
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -45,3 +59,77 @@ const restaurant = {
     },
   },
 };
+
+// The Spread Operator (...)
+const arr = [1, 2, 3];
+const newArr = [...arr, 4, 5];
+console.log(arr);
+console.log(newArr);
+
+/*
+restaurant.orderDelivery({
+  time: '20:30',
+  address: 'Jouvence, Yaounde',
+  mainIndex: 1,
+  starterIndex: 3,
+});
+
+
+// Objects Destructuring
+const { name, openingHours, categories } = restaurant;
+console.log(
+  'Destructuring using the property names',
+  name,
+  openingHours,
+  categories
+);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log('Destructuring using custom names', restaurantName, hours, tags);
+
+// default values
+const { menu = [], starterMenu: starters } = restaurant;
+console.log(menu, starters);
+
+// mutating variables
+let a = 211;
+let b = 301;
+const Obj = { a: 1, b: 3, c: 3 };
+({ a, b } = Obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open, close },
+  sat,
+} = openingHours;
+console.log(open, close, sat);
+
+// Array Destructuring
+/*
+const [starter, main] = restaurant.order(3, 1);
+console.log(starter, main);
+
+// array destructuring
+const array = [2, 3, 4];
+const [x, y, z] = array;
+console.log(x, y, z);
+
+// destructuring the first and second elements of the restaurant.categories array
+let [first, , secondary] = restaurant.categories;
+console.log(first, secondary);
+
+// swapping variables traditionally
+const temp = first;
+first = secondary;
+secondary = temp;
+console.log(first, secondary);
+
+// swapping variables using array destructuring
+[first, secondary] = [secondary, first];
+console.log(first, secondary);
+*/
