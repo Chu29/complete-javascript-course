4a363d7bbf257daad3e0a3d89533d61d63401cff
Chu A. <chu.amk22@gmail.com>
2025-09-22 06:55:20 +0100

    top level await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,21 @@
 // Importing Module
-import { addToCart, totalPrice, totalQuantity } from './shoppingCard.js';
-console.log('Importing Module');
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCard.js';
+// console.log('Importing Module');
 
-addToCart('Milk', 2);
-console.log(totalPrice, totalQuantity);
+// addToCart('Milk', 2);
+// console.log(totalPrice, totalQuantity);
+
+// top level awai
+const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await response.json();
+console.log(data);
+
+const getLastPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = await getLastPost();
+console.log(lastPost);
